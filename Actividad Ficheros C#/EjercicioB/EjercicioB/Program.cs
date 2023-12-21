20329cbbbc1b6af0ec14200438a0f3a89375de6e
IreneMoya9 <147318656+IreneMoya9@users.noreply.github.com>
2023-12-21 12:32:19 +0100

    Add files via upload

---
+++ Actividad Ficheros C#/EjercicioB/EjercicioB/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace EjercicioB
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Ejercicio B [1 pto]: Haz un programa que duplique un fichero, copiando todo 
+             * su contenido a un nuevo fichero, byte a byte. El nombre del fichero de salida 
+             * se tomará del nombre del fichero de entrada, al que se añadirá ".out". */
+
+            // Solicito introducir el nombre del fichero de entrada
+            Console.WriteLine("Introduce el nombre del fichero:");
+            string? nombreFichero = Console.ReadLine();
+
+            try
+            {
+                 
+                    // Abro el fichero de entrada en modo lectura
+                    using (FileStream ficheroEntrada = new FileStream(nombreFichero, FileMode.Open, FileAccess.Read))
+                    {
+                        // Creo el fichero de salida con el mismo nombre pero añadiendo ".out" y lo abro en modo escritura
+                        using (FileStream ficheroSalida = new FileStream(Path.GetFileNameWithoutExtension(nombreFichero) + ".out", FileMode.Create, FileAccess.Write))
+                        {
+                            int byteLeido;
+                            // Se lee byte a byte del fichero de entrada y escribe en el fichero de salida
+                            while ((byteLeido = ficheroEntrada.ReadByte()) != -1)
+                            {
+                                ficheroSalida.WriteByte((byte)byteLeido);
+                            }
+                        }
+                    }
+                
+                Console.WriteLine("Fichero duplicado correctamente");
+            }
+            catch (Exception ex)
+            {
+                // Captura y muestra cualquier excepción que pueda ocurrir
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+        }
+    }
+}
+           
+
---
+++ Actividad Ficheros C#/EjercicioC/EjercicioC/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace EjercicioC
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Ejercicio C [2 ptos]: Abre un fichero (cuyo nombre introducirá el usuario) y 
+             * muestra si se trata de un fichero bmp, gif o de otro tipo 
+             * (mostrará por consola “NombreFichero: Tipo” 
+             * [En un archivo BMP, los dos primeros bytes del archivo corresponden a una letra "B" y una letra "M",
+             * respectivamente. En un archivo GIF, los tres primeros bytes son las letras G, I, F]*/
+
+            // Solicito introducir el nombre del fichero de entrada
+            Console.WriteLine("Introduce el nombre del fichero: ");
+            string nombreFichero = Console.ReadLine();
+
+            try
+            {
+                // Abro el fichero en modo de lectura
+                using (BinaryReader fichero = new BinaryReader(File.Open(nombreFichero, FileMode.Open, FileAccess.Read)))
+                {
+                    // Leo los primeros tres bytes del fichero y lo paso a caracter
+                    char byte1 = Convert.ToChar(fichero.ReadByte());
+                    char byte2 = Convert.ToChar(fichero.ReadByte());
+                    char byte3 = Convert.ToChar(fichero.ReadByte());
+
+                    // Comparo los bytes leídos con las letras BMP y GIF
+                    if (byte1 == 'B' && byte2 == 'M' && byte3 == 'P')
+                        Console.WriteLine("El fichero" + Path.GetFileNameWithoutExtension(nombreFichero) + "es de tipo BMP");
+                    else if (byte1 == 'G' && byte2 == 'I' && byte3 == 'F')
+                        Console.WriteLine("El fichero " + Path.GetFileNameWithoutExtension(nombreFichero) + " es de tipo GIF");
+                    else
+                        Console.WriteLine("El fichero " + Path.GetFileNameWithoutExtension(nombreFichero)+" es de otro tipo");
+                }
+            }
+            catch (Exception ex)
+            {
+                // Captura y muestra cualquier excepción que pueda ocurrir
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+        }
+    }
+}
